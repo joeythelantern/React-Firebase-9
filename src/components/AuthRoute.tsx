f74a731b772a12953f2bb430d0a8397fe8409b83
saman <saman.arif@veeva.com>
2022-02-21 22:14:36 -0500

    Authcheck Fix

--- src/components/AuthRoute.tsx
+++ src/components/AuthRoute.tsx
@@ -11,18 +11,17 @@ const AuthRoute: React.FunctionComponent<IAuthRouteProps> = (props) => {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        AuthCheck();
+        const AuthCheck = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setLoading(false);
+            } else {
+                console.log('unauthorized');
+                navigate('/login');
+            }
+        });
+
         return () => AuthCheck();
     }, [auth]);
-
-    const AuthCheck = onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setLoading(false);
-        } else {
-            console.log('unauthorized');
-            navigate('/login');
-        }
-    });
 
     if (loading) return <p>loading ...</p>;
 
